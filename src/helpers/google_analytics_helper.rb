08b14adcee9e975d3f46f1b84d11121ba7dfd5e2
Chris Williams <chris@iterativedesigns.com>
2009-02-04 08:41:49 -0500

    Now with comments.

--- src/helpers/google_analytics_helper.rb
+++ src/helpers/google_analytics_helper.rb
@@ -15,4 +15,24 @@ module GoogleAnalyticsHelper
     EOF
     return script
   end
+  
+  def disqus
+    script = <<-EOF
+    <script type="text/javascript">
+    //<![CDATA[
+    (function() {
+    		var links = document.getElementsByTagName('a');
+    		var query = '?';
+    		for(var i = 0; i < links.length; i++) {
+    			if(links[i].href.indexOf('#disqus_thread') >= 0) {
+    				query += 'url' + i + '=' + encodeURIComponent(links[i].href) + '&';
+    			}
+    		}
+    		document.write('<script charset="utf-8" type="text/javascript" src="http://disqus.com/forums/jsconf2009/get_num_replies.js' + query + '"></' + 'script>');
+    	})();
+    //]]>
+    </script>
+    EOF
+    return script
+  end
 end
