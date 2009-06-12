63402186743188b3e69c2ef40b326ad0bec825a5
Chris Williams <chris@iterativedesigns.com>
2009-06-12 10:43:35 -0400

    Adding higgins and leroux.

--- src/helpers/video_helper.rb
+++ src/helpers/video_helper.rb
@@ -9,4 +9,35 @@ module VideoHelper
     <object width="400" height="300"><param name="allowfullscreen" value="true" /><param name="allowscriptaccess" value="always" /><param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=4778201&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1" /><embed src="http://vimeo.com/moogaloop.swf?clip_id=4778201&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="400" height="300"></embed></object><p><a href="http://vimeo.com/4778201">Francisco Tolmasky</a> from <a href="http://vimeo.com/jsconf">Chris Williams</a> on <a href="http://vimeo.com">Vimeo</a>.</p>
     HTML
   end
+  
+  
+  def higgins
+    str = <<-HTML
+    <object width="400" height="300"><param name="allowfullscreen" value="true" /><param name="allowscriptaccess" value="always" /><param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=4914035&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1" /><embed src="http://vimeo.com/moogaloop.swf?clip_id=4914035&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="400" height="300"></embed></object><p><a href="http://vimeo.com/4914035">Peter Higgins</a> from <a href="http://vimeo.com/jsconf">Chris Williams</a> on <a href="http://vimeo.com">Vimeo</a>.</p>
+    HTML
+  end
+  
+  
+  def leroux
+    str = <<-HTML
+    <object width="400" height="300"><param name="allowfullscreen" value="true" /><param name="allowscriptaccess" value="always" /><param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=5120343&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1" /><embed src="http://vimeo.com/moogaloop.swf?clip_id=5120343&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="400" height="300"></embed></object><p><a href="http://vimeo.com/5120343">Brian LeRoux JSConf 2009</a> from <a href="http://vimeo.com/jsconf">Chris Williams</a> on <a href="http://vimeo.com">Vimeo</a>.</p>
+    HTML
+  end
+  
+  def share_this
+    str = <<-HTML
+    <!-- AddThis Button BEGIN -->
+    <div class="addthis_toolbox addthis_default_style">
+    <a href="http://www.addthis.com/bookmark.php?v=250" class="addthis_button_compact">Share</a>
+    <span class="addthis_separator">|</span>
+    <a class="addthis_button_facebook"></a>
+    <a class="addthis_button_myspace"></a>
+    <a class="addthis_button_google"></a>
+    <a class="addthis_button_twitter"></a>
+    </div>
+    <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js?pub=voodootikigod"></script>
+    <!-- AddThis Button END -->
+    HTML
+  end
+    
 end
