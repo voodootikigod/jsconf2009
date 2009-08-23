fe8363fa4e92134d12cb2e370a1ae96b6769f0a3
Chris Williams <chris@iterativedesigns.com>
2009-08-23 14:02:40 -0400

    Adding campbell.

--- src/helpers/video_helper.rb
+++ src/helpers/video_helper.rb
@@ -4,6 +4,20 @@ module VideoHelper
     <object width="400" height="300"><param name="allowfullscreen" value="true" /><param name="allowscriptaccess" value="always" /><param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=4739942&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1" /><embed src="http://vimeo.com/moogaloop.swf?clip_id=4739942&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="400" height="300"></embed></object></p>
     HTML
   end
+
+  def campbell
+    str = <<-HTML
+    <embed src="http://blip.tv/play/g_MngZbMOwI" type="application/x-shockwave-flash" width="480" height="346" allowscriptaccess="always" allowfullscreen="true"></embed> 
+    HTML
+    str
+  end
+  def campbell
+    str = <<-HTML
+    <embed src="http://blip.tv/play/g_MngZbMOwI" type="application/x-shockwave-flash" width="480" height="346" allowscriptaccess="always" allowfullscreen="true"></embed> 
+    HTML
+    str
+  end
+  
   def duncan
     str = <<-HTML
     <embed src="http://blip.tv/play/g_MngZLzVgI" type="application/x-shockwave-flash" width="480" height="346" allowscriptaccess="always" allowfullscreen="true"></embed> 
